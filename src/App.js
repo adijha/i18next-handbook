c8770d31e478653ce34dac47e7afa3c3b85ca8a5
Aditya Kumar Jha <adityajha126@gmail.com>
2020-01-26 02:57:43 +0530

    sass also added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { useTranslation } from 'react-i18next';
 import i18next from 'i18next';
 function App() {
@@ -16,8 +16,7 @@ function App() {
 				<button onClick={() => handleClick('chi')}>Chinese</button>
 				<button onClick={() => handleClick('ne')}>Nepali</button>
 			</nav>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
+			<header className='content'>
 				<p>
 					<h3>{t('Thanks.1')}</h3> <h3>{t('Why.1')}</h3>
 				</p>
